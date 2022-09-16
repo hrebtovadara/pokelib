8934194f7dd30f019328172be0903dd1b699dfaf
d.khrebtova <d.khrebtova@headsandhands.ru>
2022-09-16 14:50:50 +0300

    test push

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mem">
+    {{store.count}}
+    <button class="t" @click="store.increment">lol</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useCounterStore } from '@/store/test'
+
+export default defineComponent ({
+  name: 'app',
+  setup() {
+    const store = useCounterStore()
+
+    return {
+      store,
+    }
+  }
+})
+</script>
+
+
+<style lang="scss">
+.mem {
+  background: red;
+
+  .t {
+    background: blue;
+  }
+}
+
+</style>
